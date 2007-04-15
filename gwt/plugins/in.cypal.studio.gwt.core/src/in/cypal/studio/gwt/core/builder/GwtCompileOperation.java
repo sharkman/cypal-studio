cae9985f44acad1a233ae63d967be05702942e98
grprakash <grprakash@8abac042-102e-0410-b702-4982c2199f88>
2007-04-15 19:27:16 +0000

    Publishing to server done!

--- gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/builder/GwtCompileOperation.java
+++ gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/builder/GwtCompileOperation.java
@@ -37,90 +37,15 @@ public class GwtCompileOperation extends PublishOperation {
 		this.project = project;
 	}
 
-//	@Override
 	public void execute(IProgressMonitor monitor, IAdaptable info) throws CoreException {
 		
 		GwtProject gwtProject = GwtProject.create(project);
 		gwtProject.doCompile();
 	}
-	
-//	
-//	private void compileModule(IFile moduleFile, IProgressMonitor monitor) throws CoreException {
-//
-//		
-////		String moduleName = Util.getSimpleName(moduleFile);
-////		String projectName = project.getName();
-////		
-////		ILaunchConfiguration launchConfig = Helper.findOrCreateLaunch(moduleName, projectName, false);
-////		ILaunchConfigurationWorkingCopy workingCopy = launchConfig.getWorkingCopy();
-////		
-////		IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
-//////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_INSTALL_NAME, vmInstall.getName());
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_JRE_CONTAINER_PATH, vmInstall.getId());
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROGRAM_ARGUMENTS, Helper.getCompilerArgs(launchConfig));
-////		
-////		
-////		List classpath = Helper.getClasspath(javaProject);
-////		
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CLASSPATH, classpath);
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_DEFAULT_CLASSPATH, false);		
-////		IFolder outputFolder = Helper.getOutputLocation(project);
-////		if(!outputFolder.exists())
-////			outputFolder.create(true, true, null);
-////		IPath outputLocation = outputFolder.getLocation();
-////		
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS, Helper.getVMArguments());
-////		workingCopy.setAttribute(IJavaLaunchConfigurationConstants.ATTR_WORKING_DIRECTORY, outputLocation.toOSString());
-////		
-////		final ILaunchConfiguration toLaunch = workingCopy.doSave(); 
-////		
-////		DebugPlugin.getDefault().addDebugEventListener(new IDebugEventSetListener() {
-////
-////			public void handleDebugEvents(DebugEvent[] events) {
-////
-////				for(int i=0;i<events.length;i++) {
-////					DebugEvent event = events[i];
-////					Object source = event.getSource();
-////					if(event.getKind() == DebugEvent.TERMINATE && source instanceof IProcess) {
-////		                ILaunch launch = ((IProcess) source).getLaunch();
-////
-////		                if(launch.getLaunchConfiguration().equals(toLaunch)) {
-////
-////							DebugPlugin.getDefault().removeDebugEventListener(this);
-////
-////							synchronized (GwtCompileOperation.this) {
-////								GwtCompileOperation.this.notify();
-////							}
-////		                }
-////					}
-////				}
-////			}
-////					
-////		});
-////
-////		DebugUITools.launch(toLaunch, ILaunchManager.RUN_MODE);
-////		
-////		try {
-////			synchronized (this) {
-////				int i = 0;
-////				while(++i < 1) {
-////					wait(5000);
-////				}
-////			}
-////		} catch (InterruptedException e) {
-////			// do nothing
-////		}
-////
-////		outputFolder.refreshLocal(IResource.DEPTH_INFINITE, monitor);
-//	}
-//
-
-//	@Override
 	public int getOrder() {
 		return -1;
 	}
 	
-//	@Override
 	public int getKind() {
 		return REQUIRED;
 	}
