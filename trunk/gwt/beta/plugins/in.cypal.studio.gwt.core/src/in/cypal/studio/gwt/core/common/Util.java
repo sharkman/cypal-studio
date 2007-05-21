a917eed5dfd17762821ec1b13b1a3854d297fe98
grprakash <grprakash@8abac042-102e-0410-b702-4982c2199f88>
2007-05-21 17:16:02 +0000

    Beta Code

---
+++ trunk/gwt/beta/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/common/Util.java
@@ -0,0 +1,350 @@
+
+
+/*
+ * Copyright 2006 Prakash (techieguy@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package in.cypal.studio.gwt.core.common;
+
+
+import in.cypal.studio.gwt.core.Activator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringBufferInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+
+
+
+/**
+ * @author Prakash (techieguy@gmail.com)
+ *
+ */
+public class Util {
+
+	
+	public static final String SRC_FOLDER = "src";
+	public static final String BIN_FOLDER = "bin";
+
+	public static IPath getGwtUserLibPath() {
+		return new Path(Constants.GWT_HOME_CPE).append("gwt-user.jar");//$NON-NLS-1$
+	}
+	
+	public static IPath getGwtDevLibPath() {
+		IPath gwtHome = JavaCore.getClasspathVariable(Constants.GWT_HOME_CPE);
+		IPath devLibPath;
+		if(Platform.getOS().equals(Platform.OS_MACOSX))
+			devLibPath = gwtHome.append("gwt-dev-mac.jar");//$NON-NLS-1$
+		else if(Platform.getOS().equals(Platform.OS_WIN32))
+			devLibPath = gwtHome.append("gwt-dev-windows.jar");//$NON-NLS-1$
+		else 
+			// the default is linux
+			devLibPath = gwtHome.append("gwt-dev-linux.jar"); //$NON-NLS-1$
+		return devLibPath;
+	}
+	
+	public static boolean isModuleXml(IResource resource) {
+		
+		boolean isModuleXml;
+		if(resource!=null && resource instanceof IFile && resource.getName().endsWith("gwt.xml"))//$NON-NLS-1$
+			isModuleXml = true;
+		else 
+			isModuleXml = false;
+		
+		return isModuleXml;
+	}
+	
+	
+	public static IJavaProject[] getGwtProjects() {
+		
+		IJavaProject[] gwtProjects = new IJavaProject[0];
+		try {
+			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+			IJavaModel javaModel = JavaCore.create(root);
+			gwtProjects = filterGwtProjects(javaModel.getJavaProjects());
+		} catch (JavaModelException e) {
+			Activator.logException(e);
+		}
+		return gwtProjects;
+	}
+	
+	public static IJavaProject[] filterGwtProjects(IJavaProject[] javaProjects) {
+
+		List gwtProjects = new ArrayList(javaProjects.length);
+
+		for (int i = 0; i < javaProjects.length; i++) {
+			
+			try {
+				if(javaProjects[i].getProject().hasNature(Constants.NATURE_ID)) {
+					gwtProjects.add(javaProjects[i]);
+				}
+			} catch (CoreException e) {
+				Activator.logException(e);
+			}
+		}
+
+		return (IJavaProject[]) gwtProjects.toArray(new IJavaProject[gwtProjects.size()]);
+
+	}
+	
+	public static String getOutputLocation(String projectName, String moduleName) {
+
+		return getOutputLocation(Util.getProject(projectName), moduleName);
+	}
+	
+	private static String getOutputLocation(IProject project, String moduleName) {
+
+		IPath outputDir = project.getLocation().append(Constants.OUTPUT_FOLDER);
+    	return outputDir.toPortableString();
+	}
+
+	public static boolean hasGwtNature(IProject project) {
+
+		boolean hasGwtNature;
+		try {
+			hasGwtNature = project.hasNature(Constants.NATURE_ID);
+		} catch (Exception e) {
+			Activator.logException(e);
+			hasGwtNature = false;
+		}
+		return hasGwtNature;
+	}
+	
+	public static boolean hasGwtNature(IJavaProject javaProject) {
+
+		boolean hasGwtNature = false;
+		try {
+			if(javaProject != null) {
+				hasGwtNature = hasGwtNature(javaProject.getProject());
+			}
+		} catch (Exception e) {
+			Activator.logException(e);
+		}
+		return hasGwtNature;
+	}
+	
+	public static IProgressMonitor getNonNullMonitor(IProgressMonitor monitor) {
+
+		if(monitor ==null)
+			monitor = new NullProgressMonitor();
+		
+		return monitor;
+	}
+
+	public static List findModules(IJavaProject javaProject) throws CoreException {
+
+		List moduleFiles = new ArrayList();
+
+		for (int i = 0;i< javaProject.getPackageFragmentRoots().length; i++) {
+
+			IPackageFragmentRoot aRoot = javaProject.getPackageFragmentRoots()[i];
+			// check only in source folders. Skip others
+			if(aRoot.getKind() != IPackageFragmentRoot.K_SOURCE) {
+				continue;
+			}
+			
+			for (int j = 0; j < aRoot.getChildren().length; j++) {
+				IJavaElement aPackage = aRoot.getChildren()[j];
+				// look only for packages. Skip others
+				if(!(aPackage instanceof IPackageFragment)) { 
+					continue;
+				}
+				Object[] nonJavaResources = ((IPackageFragment)aPackage).getNonJavaResources();
+				for (int k = 0; k < nonJavaResources.length; k++) {
+					
+					Object aResource = nonJavaResources[k];
+					// look only files. Skip others
+					if(!(aResource instanceof IFile)) {
+						continue;
+					}
+				
+					IFile aFile = (IFile) aResource;
+					if(aFile.getName().endsWith(Constants.GWT_XML_EXT)) {
+						moduleFiles.add(aFile);
+					}
+				}
+			}
+		}
+		return moduleFiles;
+	}
+	
+	public static List findRemoteServices(IJavaProject javaProject) throws CoreException {
+		
+		List remoteServiceFiles = new ArrayList();
+		
+		IPackageFragmentRoot[] packageFragmentRoots = javaProject.getPackageFragmentRoots();
+		for (int i = 0; i < packageFragmentRoots.length; i++) {
+			IPackageFragmentRoot aRoot = packageFragmentRoots[i]; 
+			// check only in source folders. Skip others
+			if(aRoot.getKind() != IPackageFragmentRoot.K_SOURCE) {
+				continue;
+			}
+			
+			IJavaElement[] children = aRoot.getChildren();
+			for (int j = 0; j < children.length; j++) {
+
+				IJavaElement aPackage = children[j];
+				// look only for packages. Skip others
+				if(!(aPackage instanceof IPackageFragment)) { 
+					continue;
+				}
+				
+				ICompilationUnit[] compilationUnits = ((IPackageFragment)aPackage).getCompilationUnits();
+				for (int k = 0; k < compilationUnits.length; k++) {
+
+					ICompilationUnit cu = compilationUnits[k];
+					
+					IResource resource = cu.getCorrespondingResource();
+					if(aPackage.getResource().getName().equals(Constants.CLIENT_PACKAGE) && resource instanceof IFile && resource.getName().endsWith(".java")){//$NON-NLS-1$
+						// java file. Check whether its a remote service ...
+
+						// for every type declared in the java file
+						IType[] types = cu.getTypes();
+						for (int l = 0; l < types.length; l++) {
+							IType someType = types[l];
+							// for every interface implemented by that type
+							
+							String[] superInterfaceNames = someType.getSuperInterfaceNames();
+							for (int m = 0; m < superInterfaceNames.length; m++) {
+								String aSuperInterface = superInterfaceNames[m];
+								if(aSuperInterface.equals(Constants.REMOTE_SERVICE_CLASS)) {
+									remoteServiceFiles.add((IFile)resource);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return remoteServiceFiles;
+	}
+	
+	public static boolean isRemoteService(IResource resource) throws JavaModelException {
+		
+		boolean isRemoteService = false;
+		if(resource != null && resource instanceof IFile && resource.getName().endsWith(".java")) {//$NON-NLS-1$
+			
+			ICompilationUnit cu = (ICompilationUnit) JavaCore.create(resource);
+			isRemoteService = isRemoteService(cu);
+		}
+		
+		return isRemoteService;
+	}
+	
+	
+	public static boolean isRemoteService(ICompilationUnit cu) throws JavaModelException {
+		
+		boolean isRemoteService = false;
+		if(cu != null) {
+			IType[] types = cu.getTypes();
+			for (int i = 0; i < types.length; i++) {
+				IType someType = types[i];
+				// for every interface implemented by that type
+				String[] superInterfaceNames = someType.getSuperInterfaceNames();
+				for (int j = 0; j < superInterfaceNames.length; j++) {
+					String aSuperInterface = superInterfaceNames[j];
+					if(aSuperInterface.equals(Constants.REMOTE_SERVICE_CLASS)) {
+						isRemoteService = true;
+					}
+				}
+			}
+		}
+		return isRemoteService;
+	}
+	
+	public static boolean isGwtModuleFile(IFile file) {
+		
+		boolean isGwtModuleFile;
+		if(file != null && file.getName().endsWith(Constants.GWT_XML_EXT)){
+			isGwtModuleFile = true;
+		}else {
+			isGwtModuleFile = false;
+		}
+		
+		return isGwtModuleFile;
+	}
+	
+	public static String getSimpleName(IFile file) {
+		
+		String simpleName = "";//$NON-NLS-1$
+		if(file!=null) {
+			simpleName = file.getName();
+			int index = simpleName.indexOf(Constants.GWT_XML_EXT);
+			simpleName = simpleName.substring(0, index-1);
+		}
+		return simpleName;
+	}
+	
+	public static String getQualifiedName(IFile file) {
+		
+		String qualifiedName = "";//$NON-NLS-1$
+		if(file!=null) {
+			StringBuilder builder = new StringBuilder();
+			String [] segments = file.getProjectRelativePath().segments();
+			for(int i = 1; i<segments.length;i++) {
+				builder.append(segments[i]);
+				builder.append('.');
+			}
+			qualifiedName = builder.substring(0, builder.length()-Constants.GWT_XML_EXT.length()-2);
+		}
+		return qualifiedName;
+	
+	}
+	
+	public static boolean isHosted(IProject project) {
+
+		boolean isHosted = true;
+		try {
+			return Boolean.parseBoolean(project.getPersistentProperty(Constants.IS_HOSTED_DEPLOY_MODE));
+		} catch (CoreException e) {
+			Activator.logException(e);
+		}
+		return isHosted;
+	}
+	
+	public static IProject getProject(String name) {
+		return ResourcesPlugin.getWorkspace().getRoot().getProject(name);
+	}
+
+}
