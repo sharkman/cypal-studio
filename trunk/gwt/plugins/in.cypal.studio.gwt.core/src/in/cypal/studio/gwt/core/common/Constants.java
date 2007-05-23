f7d9d6692110eb8d1ad4ce341b651d7870537c00
grprakash <grprakash@8abac042-102e-0410-b702-4982c2199f88>
2007-05-23 15:53:04 +0000

    Adding support for Eclipse 3.3

--- trunk/gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/common/Constants.java
+++ trunk/gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/common/Constants.java
@@ -1,10 +1,27 @@
+/*
+ * Copyright 2006 Cypal Solutions (tools@cypal.in)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package in.cypal.studio.gwt.core.common;
 
 import in.cypal.studio.gwt.core.Activator;
 
-import org.eclipse.core.runtime.QualifiedName;
-import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
-
+/**
+ * @author Prakash G.R.
+ *
+ */
 public class Constants {
 	
 	public static final String PLUGIN_ID = Activator.PLUGIN_ID;
@@ -31,8 +48,8 @@ public class Constants {
 	public static final String LAUNCH_ATTR_OUTDIR = PLUGIN_ID+".launchAttrOutDir";//$NON-NLS-1$
 	public static final String LAUNCH_ATTR_STYLE = PLUGIN_ID+".launchAttrStyle";//$NON-NLS-1$
 	public static final String LAUNCH_ATTR_VMOPTIONS = PLUGIN_ID+".launchVmOptions";//$NON-NLS-1$
-	public static final String LAUNCH_ATTR_MAIN_TYPE = IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME;
-	public static final String LAUNCH_ATTR_PROJECT_NAME = IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME;
+	public static final String LAUNCH_ATTR_MODULE_NAME = PLUGIN_ID+".launchModuleName";//$NON-NLS-1$
+	public static final String LAUNCH_ATTR_PROJECT_NAME = PLUGIN_ID+".launchProjectName";//$NON-NLS-1$
 
 	
 	//Folders
@@ -53,7 +70,7 @@ public class Constants {
 
 	
 	public static final String GWT_HOME_CPE = "GWT_HOME";
-	public static final QualifiedName IS_HOSTED_DEPLOY_MODE = null;
+//	public static final QualifiedName IS_HOSTED_DEPLOY_MODE = null;
 	public static final String GWT_HOME_PATH = "GWT_HOME";
 
 
