5f1c6babc5f2994fc69770d3bbeeab25844fd5fb
grprakash <grprakash@8abac042-102e-0410-b702-4982c2199f88>
2007-05-23 15:54:07 +0000

    Adding support for Eclipse 3.3

--- trunk/gwt/plugins/in.cypal.studio.gwt.ui/src/in/cypal/studio/gwt/ui/launch/MainTab.java
+++ trunk/gwt/plugins/in.cypal.studio.gwt.ui/src/in/cypal/studio/gwt/ui/launch/MainTab.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006 Prakash (techieguy@gmail.com)
+ * Copyright 2006 Cypal Solutions (tools@cypal.in)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,165 +19,156 @@ package in.cypal.studio.gwt.ui.launch;
 
 import in.cypal.studio.gwt.ui.Activator;
 import in.cypal.studio.gwt.ui.common.Constants;
-import in.cypal.studio.gwt.ui.common.GwtLabelProvider;
 import in.cypal.studio.gwt.ui.common.Util;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.internal.debug.ui.IJavaDebugHelpContextIds;
-import org.eclipse.jdt.internal.debug.ui.launcher.SharedJavaMainTab;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.ui.ISharedImages;
-import org.eclipse.jdt.ui.JavaElementLabelProvider;
 import org.eclipse.jdt.ui.JavaUI;
-import org.eclipse.jface.viewers.ILabelProvider;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.swt.widgets.Label;
 
 /**
  * 
- * @author Prakash (techieguy@gmail.com)
- *
+ * @author Prakash G.R.
+ * 
  */
-//@SuppressWarnings("restriction") 
-public class MainTab extends SharedJavaMainTab {
-	
-//	@Override
-	protected void handleProjectButtonSelected() {
-		IJavaProject project = chooseJavaProject();
-		if (project != null) {
-			fProjText.setText(project.getElementName());
-		}
-	}
-	
-	private IJavaProject chooseJavaProject() {
+public class MainTab extends AbstractLaunchConfigurationTab {
 
-		ILabelProvider labelProvider= new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT);
-		ElementListSelectionDialog dialog= new ElementListSelectionDialog(getShell(), labelProvider);
-		dialog.setTitle("Project Selection"); 
-		dialog.setMessage("Select a Gwt project to constrain your search.");
-		dialog.setElements(Util.getGwtProjects());
-
-		IJavaProject javaProject= getJavaProject();
-		if (javaProject != null) {
-			dialog.setInitialSelections(new Object[] { javaProject });
-		}
-
-		IJavaProject result = null;
-		if (dialog.open() == Window.OK) {			
-			result = (IJavaProject) dialog.getFirstResult();
-		}
-		return result;		
-	}
+	String projectName;
+	String moduleName;
+	private Combo projectCombo;
+	private Combo moduleCombo;
 
 	public void createControl(Composite parent) {
-		Font font= parent.getFont();
-		Composite projComp= new Composite(parent, SWT.NONE);
-		setControl(projComp);
-		PlatformUI.getWorkbench().getHelpSystem().setHelp(getControl(), IJavaDebugHelpContextIds.LAUNCH_CONFIGURATION_DIALOG_APPLET_MAIN_TAB);
-		GridLayout projLayout= new GridLayout();
-		projLayout.verticalSpacing = 0;
-		projComp.setLayout(projLayout);
-		projComp.setFont(font);
-		createProjectEditor(projComp);
-		createVerticalSpacer(projComp, 1);
-		createMainTypeEditor(projComp, "&Module:", null);
-		createVerticalSpacer(projComp, 1);
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(2, false));
+
+		Label projectLabel = new Label(composite, SWT.NONE);
+		projectLabel.setText("Project:");
+
+		projectCombo = new Combo(composite, SWT.READ_ONLY);
+		projectCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		projectCombo.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				updateModule();
+				updateLaunchConfigurationDialog();
+			}
+		});
+		
+		Label moduleLabel = new Label(composite, SWT.NONE);
+		moduleLabel.setText("Module:");
+
+		moduleCombo = new Combo(composite, SWT.READ_ONLY);
+		moduleCombo.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+		moduleCombo.addSelectionListener(new SelectionAdapter() {
+
+			public void widgetSelected(SelectionEvent e) {
+				updateLaunchConfigurationDialog();
+			}
+		});
+		
+		init();
+		
+		setControl(composite);
+	}
+
+	private void init() {
+
+		IJavaProject[] gwtProjects = Util.getGwtProjects();
+		for (int i = 0; i < gwtProjects.length; i++) {
+			projectCombo.add(gwtProjects[i].getElementName());
+			projectCombo.setData(gwtProjects[i].getElementName(), gwtProjects[i]);
+		}
 		
 	}
-	
-	public Image getImage() {
-		return JavaUI.getSharedImages().getImage(ISharedImages.IMG_OBJS_CLASS);
+
+	protected void updateModule() {
+
+		moduleCombo.removeAll();
+		List modules;
+		try {
+			IJavaProject project = (IJavaProject) projectCombo.getData(projectCombo.getText());
+			modules = Util.findModules(project);
+			for (Iterator i = modules.iterator(); i.hasNext();) {
+				IFile moduleFile = (IFile) i.next();
+				moduleCombo.add(Util.getQualifiedName(moduleFile));
+			}
+			
+			if(modules.size() > 0) {
+				moduleCombo.select(0);
+			}
+
+		} catch (Exception e) {
+			Activator.logException(e);
+		}
+		
 	}
-	
+
 	public String getName() {
 		return "Main";
 	}
 
-	protected void handleSearchButtonSelected() {
-		
-		IJavaProject javaProject = getJavaProject();
-		if(!Util.hasGwtNature(javaProject)) {
-			setErrorMessage("Please select a valid GWT project to search");
-		}else {
-			try {
-				ElementListSelectionDialog dialog = new ElementListSelectionDialog(getShell(), new GwtLabelProvider());
-				dialog.setTitle("Select module:"); 
-				dialog.setMessage("Select a module to run.");
-				List moduleFiles = Util.findModules(javaProject);
-				dialog.setElements(moduleFiles.toArray(new IFile[moduleFiles.size()]));
-				if(dialog.open() == Window.OK) {
-					IFile selectedModule = (IFile) dialog.getFirstResult();
-					String moduleName = Util.getQualifiedName(selectedModule);
-					fMainText.setText(moduleName);
-				}
-			}catch(CoreException e) {
-				Activator.handleException(e);
-			}
-		}
-	}
-	
-	public void initializeFrom(ILaunchConfiguration config) {
-		super.initializeFrom(config);
-		updateMainTypeFromConfig(config);
+	public Image getImage() {
+		return JavaUI.getSharedImages().getImage(ISharedImages.IMG_OBJS_CLASS);
 	}
 
-	public boolean isValid(ILaunchConfiguration launchConfig) {
-		
-		boolean isValid = false; 
-		setErrorMessage(null);
-		setMessage(null);
-		String name= fProjText.getText().trim();
-		if(name.length() == 0) {
-			setErrorMessage("Please specify a project");
-		}else {
-			
-			isValid = isValidProject();
-		}
-
-		return isValid;
-	}
-	
-	
-	public boolean isValidProject() {
-		
-		boolean isValid = false;
-		IProject project = Util.getProject(fProjText.getText().trim());
+	public void initializeFrom(ILaunchConfiguration configuration) {
+		String projectName = "";
+		String moduleName = "";
 		try {
-			if(!project.exists()) {
-				setErrorMessage("The specified project does not exist");
-			}else if(project.getNature(Constants.NATURE_ID)==null) {
-				setErrorMessage("The specified project is not a valid GWT project");
-			}else {
-				// its a valid project with GWT nature
-				isValid = true;
+			projectName = configuration.getAttribute(Constants.LAUNCH_ATTR_PROJECT_NAME, "");	
+			moduleName = configuration.getAttribute(Constants.LAUNCH_ATTR_MODULE_NAME, "");
+			if(projectName.equals("")) {
+				// support previous versions...
+				projectName = configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, "");
 			}
-		} catch (CoreException e) {
-			Activator.logException(e);
 		}
-		return isValid;
+		catch (CoreException ce) {
+			setErrorMessage(ce.getStatus().getMessage());
+		}
+		projectCombo.setText(projectName);
+		updateModule();
+		moduleCombo.setText(moduleName);
 	}
 
-	public void performApply(ILaunchConfigurationWorkingCopy config) {
-		config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, fProjText.getText());
-		config.setAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, fMainText.getText());
-		mapResources(config);
+	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
+		
+		configuration.setAttribute(Constants.LAUNCH_ATTR_PROJECT_NAME, projectCombo.getText());
+		configuration.setAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, projectCombo.getText());
+		configuration.setAttribute(Constants.LAUNCH_ATTR_MODULE_NAME, moduleCombo.getText());
 	}
-	
-	public void setDefaults(ILaunchConfigurationWorkingCopy config) {
 
+	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
+
+		IJavaProject[] gwtProjects = Util.getGwtProjects();
+		if(gwtProjects.length > 0) {
+			projectName = gwtProjects[0].getElementName();
+			try {
+				List modules = Util.findModules(gwtProjects[0]);
+				if(modules.size() > 0) {
+					moduleName = Util.getQualifiedName((IFile) modules.get(0));
+				}
+			} catch (CoreException e) {
+				Activator.logException(e);
+			}
+		}
 	}
+
 
 }
