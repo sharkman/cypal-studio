f4eeedd708ed3be396c4c884a3956dade4d8158d
grprakash <grprakash@8abac042-102e-0410-b702-4982c2199f88>
2008-06-03 10:18:45 +0000

    Issue#72 publishing occurs twice

--- trunk/gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/common/GwtProject.java
+++ trunk/gwt/plugins/in.cypal.studio.gwt.core/src/in/cypal/studio/gwt/core/common/GwtProject.java
@@ -33,6 +33,7 @@ import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.core.ILaunchManager;
@@ -73,8 +74,8 @@ public class GwtProject {
 		}
 		return moduleFiles;
 	}
-	
-	public List getRemoteServices(){
+
+	public List getRemoteServices() {
 
 		if (remoteServiceFiles == null) {
 			try {
@@ -86,12 +87,12 @@ public class GwtProject {
 		}
 		return remoteServiceFiles;
 	}
-	
+
 	public void doCompile() {
-		
+
 		List modules = getModules();
 		for (Iterator i = modules.iterator(); i.hasNext();) {
-			IFile aModule  = (IFile) i.next();
+			IFile aModule = (IFile) i.next();
 			try {
 				compileModule(aModule);
 			} catch (CoreException e) {
@@ -99,7 +100,7 @@ public class GwtProject {
 				Activator.logException(e);
 			}
 		}
-		
+
 		IFolder outputFolder = getProject().getFolder(Util.getGwtOutputFolder());
 		try {
 			outputFolder.refreshLocal(IResource.DEPTH_INFINITE, null);
@@ -110,129 +111,153 @@ public class GwtProject {
 	}
 
 	public void doClean(IProgressMonitor monitor) throws CoreException {
-		
+
 		monitor = Util.getNonNullMonitor(monitor);
 		try {
 			List modules = getModules();
-			monitor.beginTask("Starting clean...", modules.size()*2);
+			monitor.beginTask("Starting clean...", modules.size() * 2);
 			IFolder folder = project.getFolder(Util.getGwtOutputFolder());
-			for(int i=0;i<modules.size();i++) {
+			for (int i = 0; i < modules.size(); i++) {
 				IFile aModuleFile = (IFile) modules.get(i);
 				String moduleName = Util.getQualifiedName(aModuleFile);
 				IFolder moduleOutputFolder = folder.getFolder(moduleName);
-				if(moduleOutputFolder.exists()) {
-					moduleOutputFolder.delete(true, new SubProgressMonitor(monitor, (i*2)+1));
-					moduleOutputFolder.refreshLocal(IResource.DEPTH_ONE, new SubProgressMonitor(monitor, (i*2)+2));
+				if (moduleOutputFolder.exists()) {
+					moduleOutputFolder.delete(true, new SubProgressMonitor(monitor, (i * 2) + 1));
+					moduleOutputFolder.refreshLocal(IResource.DEPTH_ONE, new SubProgressMonitor(monitor, (i * 2) + 2));
 				}
 			}
-		}finally {
+		} finally {
 			monitor.done();
 		}
-		
+
 	}
-	
+
 	private void compileModule(IFile moduleFile) throws CoreException {
-		
+
 		String moduleName = Util.getQualifiedName(moduleFile);
 		String projectName = project.getName();
 		IProject project = Util.getProject(projectName);
-		
+
 		Util.createModuleEntry(project, moduleName);
-		
+
 		ILaunchConfiguration launchConfig = Helper.findOrCreateLaunch(moduleName, projectName, true);
 		ILaunchConfigurationWorkingCopy workingCopy = launchConfig.getWorkingCopy();
 		workingCopy.setAttribute(Constants.LAUNCH_ATTR_GWT_COMPILE, true);
-//		workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
-		workingCopy.launch(ILaunchManager.RUN_MODE, null, false, true );
-//		launch(launchConfig);
-		
+		// workingCopy.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, true);
+		ILaunch compiler = workingCopy.launch(ILaunchManager.RUN_MODE, null, false, true);
+		// launch(launchConfig);
+
+		try {
+			synchronized (this) {
+				int i = 0;
+				while ((!compiler.isTerminated()) && (i < 90)) {
+					wait(1000);
+					i++;
+				}
+				// TODO: we throw an exception on timeout?
+			}
+		} catch (InterruptedException e) {
+			// ok;
+		}
+
+		IFolder outputLocation = Helper.getOutputLocation(project);
+		outputLocation.refreshLocal(IResource.DEPTH_INFINITE, null);
+
 	}
-//
-//	private void launch(ILaunchConfiguration launchConfig) throws CoreException {
-//		
-//		IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
-//		IVMRunner vmRunner = vmInstall.getVMRunner(ILaunchManager.RUN_MODE);
-//		
-//		List classpath = Helper.getClasspath(javaProject);
-//		IPackageFragmentRoot[] packageFragmentRoots = javaProject.getPackageFragmentRoots();
-//		for (int i = 0; i < packageFragmentRoots.length; i++) {
-//
-//			IPackageFragmentRoot aRoot = packageFragmentRoots[i];
-//			if(aRoot.getKind() == IPackageFragmentRoot.K_SOURCE) {
-//				IResource resource = aRoot.getResource();
-//				IPath location = resource.getLocation();
-//				classpath.add(location.toOSString());
-//			}
-//		}
-//			
-//		VMRunnerConfiguration vmConfig = new VMRunnerConfiguration(Constants.GWT_COMPILER_CLASS, (String[]) classpath.toArray(new String[classpath.size()]));
-//		vmConfig.setWorkingDirectory(project.getLocation().toOSString());
-//		List compilerArgs = Helper.getCompilerArgs(launchConfig);
-//		vmConfig.setProgramArguments((String[]) compilerArgs.toArray(new String[compilerArgs.size()]));
-//		final ILaunch compiler = new Launch(null, ILaunchManager.RUN_MODE, null);
-//		compiler.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, "true");
-//		compiler.setAttribute(Constants.LAUNCH_ATTR_GWT_COMPILE, "true");
-//		
-//		DebugPlugin.getDefault().addDebugEventListener(new IDebugEventSetListener() {
-//
-//			public void handleDebugEvents(DebugEvent[] events) {
-//
-//				for(int i = 0;i<events.length;i++) {
-//
-//					DebugEvent event = events[i];
-//					
-//		            Object source = event.getSource();
-//		            if (source instanceof IProcess && event.getKind() == DebugEvent.TERMINATE) {
-//
-//				        ILaunch launch = ((IProcess) source).getLaunch();
-//				        if (compiler.equals(launch)) {
-//				        	DebugPlugin.getDefault().removeDebugEventListener(this);
-//
-//							// wakeup the publisher
-//							synchronized (GwtProject.this) {
-//								GwtProject.this.notify();
-//							}
-//		                }
-//					}
-//				
-//				}
-//			}
-//		});
-//
-//		vmRunner.run(vmConfig, compiler, null);
-//		
-//					
-//		try {
-//			synchronized (this) {
-//				int i = 0;
-//				while(!compiler.isTerminated() && i < 8) {
-//					wait(5000);
-//					i++;
-//				}
-//				// TODO: we throw an exception on timeout?
-//			}
-//		} catch (InterruptedException e) {
-//			// ok;
-//		}
-//					
-//		IFolder outputLocation = Helper.getOutputLocation(project);
-//		outputLocation.refreshLocal(IResource.DEPTH_INFINITE, null);
-//		
-//	}
+
+	//
+	// private void launch(ILaunchConfiguration launchConfig) throws
+	// CoreException {
+	//		
+	// IVMInstall vmInstall = JavaRuntime.getDefaultVMInstall();
+	// IVMRunner vmRunner = vmInstall.getVMRunner(ILaunchManager.RUN_MODE);
+	//		
+	// List classpath = Helper.getClasspath(javaProject);
+	// IPackageFragmentRoot[] packageFragmentRoots =
+	// javaProject.getPackageFragmentRoots();
+	// for (int i = 0; i < packageFragmentRoots.length; i++) {
+	//
+	// IPackageFragmentRoot aRoot = packageFragmentRoots[i];
+	// if(aRoot.getKind() == IPackageFragmentRoot.K_SOURCE) {
+	// IResource resource = aRoot.getResource();
+	// IPath location = resource.getLocation();
+	// classpath.add(location.toOSString());
+	// }
+	// }
+	//			
+	// VMRunnerConfiguration vmConfig = new
+	// VMRunnerConfiguration(Constants.GWT_COMPILER_CLASS, (String[])
+	// classpath.toArray(new String[classpath.size()]));
+	// vmConfig.setWorkingDirectory(project.getLocation().toOSString());
+	// List compilerArgs = Helper.getCompilerArgs(launchConfig);
+	// vmConfig.setProgramArguments((String[]) compilerArgs.toArray(new
+	// String[compilerArgs.size()]));
+	// final ILaunch compiler = new Launch(null, ILaunchManager.RUN_MODE, null);
+	// compiler.setAttribute(DebugPlugin.ATTR_CAPTURE_OUTPUT, "true");
+	// compiler.setAttribute(Constants.LAUNCH_ATTR_GWT_COMPILE, "true");
+	//		
+	// DebugPlugin.getDefault().addDebugEventListener(new
+	// IDebugEventSetListener() {
+	//
+	// public void handleDebugEvents(DebugEvent[] events) {
+	//
+	// for(int i = 0;i<events.length;i++) {
+	//
+	// DebugEvent event = events[i];
+	//					
+	// Object source = event.getSource();
+	// if (source instanceof IProcess && event.getKind() ==
+	// DebugEvent.TERMINATE) {
+	//
+	// ILaunch launch = ((IProcess) source).getLaunch();
+	// if (compiler.equals(launch)) {
+	// DebugPlugin.getDefault().removeDebugEventListener(this);
+	//
+	// // wakeup the publisher
+	// synchronized (GwtProject.this) {
+	// GwtProject.this.notify();
+	// }
+	// }
+	// }
+	//				
+	// }
+	// }
+	// });
+	//
+	// vmRunner.run(vmConfig, compiler, null);
+	//		
+	//					
+	// try {
+	// synchronized (this) {
+	// int i = 0;
+	// while(!compiler.isTerminated() && i < 8) {
+	// wait(5000);
+	// i++;
+	// }
+	// // TODO: we throw an exception on timeout?
+	// }
+	// } catch (InterruptedException e) {
+	// // ok;
+	// }
+	//					
+	// IFolder outputLocation = Helper.getOutputLocation(project);
+	// outputLocation.refreshLocal(IResource.DEPTH_INFINITE, null);
+	//		
+	// }
 
 	public IProject getProject() {
 		return project;
 	}
-	
+
 	public IJavaProject getJavaProject() {
 		return javaProject;
 	}
 
 	public List getRemoteServices(IResourceDelta delta) {
-		
-		if(delta == null) {
+
+		if (delta == null) {
 			remoteServiceFiles = getRemoteServices();
-		}else {
+		} else {
 			remoteServiceFiles = new ArrayList();
 			try {
 				delta.accept(new ResourceDeltaVisitor());
@@ -242,8 +267,8 @@ public class GwtProject {
 		}
 		return remoteServiceFiles;
 	}
-	
-	private class ResourceDeltaVisitor implements IResourceDeltaVisitor{
+
+	private class ResourceDeltaVisitor implements IResourceDeltaVisitor {
 
 		public boolean visit(IResourceDelta delta) throws CoreException {
 			boolean interested = true;
